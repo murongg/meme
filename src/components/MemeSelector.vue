702dcbd9ac30d288d732193186b4a08a992d4124
木荣 <372638156@qq.com>
2022-08-01 18:32:36 +0800

    feat: add loading

--- src/components/MemeSelector.vue
+++ src/components/MemeSelector.vue
@@ -5,6 +5,7 @@ const currentIndex = ref(0)
 const currentCategoryIndex = ref(0)
 const categories = ref<any[]>([])
 const materials = ref<any[]>([])
+const isFinished = ref(false)
 
 function itemClick(index: number) {
   currentIndex.value = index
@@ -18,6 +19,7 @@ function categoryClick(index: number, cate: string) {
 }
 
 async function getData(cate: string, cate2 = '') {
+  isFinished.value = false
   let url = `/data/${cate}`
   if (cate2)
     url += `/${cate2}/index.json`
@@ -25,9 +27,10 @@ async function getData(cate: string, cate2 = '') {
     url += '/index.json'
 
   const fetch = await useFetch(url).json
-  const data = await fetch()
-  materials.value = data.data.value.materials
-  categories.value = data.data.value.categories
+  const { data } = await fetch()
+  isFinished.value = true
+  materials.value = data.value.materials
+  categories.value = data.value.categories
 }
 
 onMounted(() => {
@@ -45,36 +48,31 @@ const { list: categoryList, containerProps: categoryContainerProps, wrapperProps
 <template>
   <div overflow="hidden" h="screen">
     <ul flex w="full" mb="10px">
-      <li
-        v-for="(category, index) in CATEGORYS" :key="category" w="20%" cursor="pointer"
+      <li v-for="(category, index) in CATEGORYS" :key="category" w="20%" cursor="pointer"
         :text="currentIndex === index && `teal-700`" :border-r="index !== CATEGORYS.length - 1 && `1`"
-        border-color="light" @click="itemClick(index)"
-      >
+        border-color="light" @click="itemClick(index)">
         {{ category }}
       </li>
     </ul>
     <div flex>
       <div v-bind="categoryContainerProps as any" important-w="30%">
         <ul v-bind="categoryWrapperProps" overflow-y="scroll" important-h="760px">
-          <li
-            v-for="(data, index) in categoryList" :key="data.data.path" h="40px" leading="40px" border-color="light"
+          <li v-for="(data, index) in categoryList" :key="data.data.path" h="40px" leading="40px" border-color="light"
             cursor="pointer" :text="currentCategoryIndex === index && `teal-700`"
-            :border-b="index !== CATEGORYS.length - 1 && `1`" @click="categoryClick(index, data.data.cate2)"
-          >
+            :border-b="index !== CATEGORYS.length - 1 && `1`" @click="categoryClick(index, data.data.cate2)">
             {{ data.data.cate2 }}
           </li>
         </ul>
       </div>
-      <div important-w="70%" overflow-y="auto">
+      <div important-w="70%" overflow-y="auto" v-if="isFinished">
         <ul overflow-y="scroll" h="760px" text="left">
-          <li
-            v-for="data in materials" :key="data.path" inline-block w="50%" px="2%" py="%2" mb="10px" box-border
-            v="middle"
-          >
+          <li v-for="data in materials" :key="data.path" inline-block w="50%" px="2%" py="%2" mb="10px" box-border
+            v="middle">
             <img v-lazy="data.path">
           </li>
         </ul>
       </div>
+      <span v-else>loading</span>
     </div>
   </div>
 </template>
