934900bf17af80176d17e463ee2070454dafa9e0
木荣 <372638156@qq.com>
2022-08-02 17:50:18 +0800

    refactor: reset download image

--- src/components/MemeSelector.vue
+++ src/components/MemeSelector.vue
@@ -50,10 +50,10 @@ const { list: categoryList, containerProps: categoryContainerProps, wrapperProps
   },
 )
 
-function onDrag(event: DragEvent, data: any) {
+async function onDrag(event: DragEvent, data: any) {
   const imgElement = new Image()
   imgElement.src = data.path
-  // imgElement.setAttribute("crossOrigin", "anonymous")
+
   imgElement.onload = () => {
     const image = new fabric.Image(imgElement)
     const imageSize = 200
@@ -75,32 +75,26 @@ function onDrag(event: DragEvent, data: any) {
 <template>
   <div overflow="hidden" h="screen">
     <ul flex w="full" mb="10px">
-      <li
-        v-for="(category, index) in CATEGORYS" :key="category" w="20%" cursor="pointer"
+      <li v-for="(category, index) in CATEGORYS" :key="category" w="20%" cursor="pointer"
         :text="currentIndex === index && `teal-700`" :border-r="index !== CATEGORYS.length - 1 && `1`"
-        border-color="light" @click="itemClick(index)"
-      >
+        border-color="light" @click="itemClick(index)">
         {{ category }}
       </li>
     </ul>
     <div flex>
       <div v-bind="categoryContainerProps as any" important-w="22%">
         <ul v-bind="categoryWrapperProps" overflow-y="scroll" important-h="760px">
-          <li
-            v-for="(data, index) in categoryList" :key="data.data.path" h="40px" leading="40px" border-color="light"
+          <li v-for="(data, index) in categoryList" :key="data.data.path" h="40px" leading="40px" border-color="light"
             cursor="pointer" :text="currentCategoryIndex === index && `teal-700`"
-            :border-b="index !== CATEGORYS.length - 1 && `1`" @click="categoryClick(index, data.data.cate2)"
-          >
+            :border-b="index !== CATEGORYS.length - 1 && `1`" @click="categoryClick(index, data.data.cate2)">
             {{ data.data.cate2 }}
           </li>
         </ul>
       </div>
       <div v-if="isFinished" important-w="73%" overflow-y="auto" ml="5%">
         <ul overflow-y="scroll" h="760px" text="left">
-          <li
-            v-for="data in materials" :key="data.path" cursor="pointer" inline-block w="50%" px="2%" py="%2" mb="10px"
-            box-border v="middle"
-          >
+          <li v-for="data in materials" :key="data.path" cursor="pointer" inline-block w="50%" px="2%" py="%2" mb="10px"
+            box-border v="middle">
             <img v-lazy="data.path" draggable @dragend="onDrag($event, data)">
           </li>
         </ul>
